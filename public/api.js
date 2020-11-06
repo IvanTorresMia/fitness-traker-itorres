706a0fe12ba840f9068b4cdd6ad44d41a81880cd
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-05 19:13:39 -0800

    added starting templates

---
+++ public/api.js
@@ -0,0 +1,48 @@
+
+const API = {
+  // get the last work out call. 
+  async getLastWorkout() {
+    let res;
+    try {
+      res = await fetch("/api/workouts");
+    } catch (err) {
+      console.log(err)
+    }
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  // add an exersice 
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  // create a work out 
+  async createWorkout(data = {}) {
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  // getting workout in range 
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+
+    return json;
+  },
+};
