7ec63b364f8b39f97ad1467d2ce499ea8c9a75ee
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-06 21:10:57 -0800

    added virtuals

--- models/workout.js
+++ models/workout.js
@@ -27,8 +27,24 @@ const workoutSchema = new Schema({
       distance: Number,
     },
   ],
-});
+
+} ,
+{
+    toJSON: {
+      virtuals: true
+    }
+  } 
+
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce(function (total, exercise) {
+        return total + exercise.duration;
+      }, 0);
+  });
 
 const Workout = mongoose.model("workout", workoutSchema);
 
+
 module.exports = Workout;
+
